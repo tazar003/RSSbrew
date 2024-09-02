bc2b6481f9134aa8bfd3681cb1ba63730634911c
tazar003 <thomas.azar@gmail.com>
2024-09-02 06:28:59 -0500

    Updated settings

--- rssbrew/settings.py
+++ rssbrew/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', get_random_secret_key())
 
 # SECURITY WARNING: Manuall enable debug!
-DEBUG = True
+DEBUG = False
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
